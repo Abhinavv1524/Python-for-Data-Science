b6424d3bd2b5c7a44b4b53a50ee7b0ff7a802452
Abhinavv1524 <choudharyabhinav818@gmail.com>
2025-02-05 11:06:17 +0530

    Colored Text

---
+++ projects/10.Colored Print.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Abhinav Choudhary "+ Fore.YELLOW+ Back.BLUE+"I am a Student")
+print(Back.CYAN+"Hi My name is Abhinav Choudhary")
+print(Fore.RED + Back.GREEN+ "Hi My name is Abhinav Choudhary")
